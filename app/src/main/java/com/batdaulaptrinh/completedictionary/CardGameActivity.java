efdba0d936ebb5cd783899c3968b165c32604815
SIMP lỏd <67738980+19522541@users.noreply.github.com>
2021-06-04 16:46:03 +0700

    add_card_game

--- app/src/main/java/com/batdaulaptrinh/completedictionary/CardGameActivity.java
+++ app/src/main/java/com/batdaulaptrinh/completedictionary/CardGameActivity.java
@@ -1,21 +1,68 @@
 package com.batdaulaptrinh.completedictionary;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class CardGameActivity extends AppCompatActivity {
 
     ImageButton backBt;
+    TextView countCart;
+    TextView textWord;
+    TextView ipa_usView;
+    TextView ipa_ukView;
+    Button showButton;
+    LinearLayout descirbeLayout;
+    TextView textExample;
+    TextView typeTextView;
+    public   String  enDefinition;
+    public  String  example;
+    public String type;
+    public String ipa_us;
+    public String ipa_uk;
+    public  Boolean show;
+    public int  stt;
+    static com.batdaulaptrinh.completedictionary.DatabaseHelper myDbHelper;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_game);
         backBt = findViewById(R.id.backButton);
+        descirbeLayout= (LinearLayout)findViewById(R.id.describeBox);
         Intent mainIt = new Intent(this,MainActivity.class);
+        showButton =(Button)findViewById(R.id.showButton);
+        myDbHelper = new com.batdaulaptrinh.completedictionary.DatabaseHelper(this);
+
+        if (myDbHelper.checkDataBase()) {
+            openDatabase();
+
+        } else {
+            com.batdaulaptrinh.completedictionary.LoadDatabaseAsync task = new com.batdaulaptrinh.completedictionary.LoadDatabaseAsync(CardGameActivity.this);
+            task.execute();
+        }
+      Cursor c = myDbHelper.getMeaning("H");
+      //  DatabaseUtils.dumpCursorToString(c);
+     //   DatabaseUtils.dumpCursorToString(c);
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                descirbeLayout.setVisibility(View.INVISIBLE);
+            }
+        });
         backBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,5 +70,13 @@ public class CardGameActivity extends AppCompatActivity {
                 startActivity(mainIt);
             }
         });
+    }
+
+    private void openDatabase() {
+        try {
+            myDbHelper.openDataBase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
