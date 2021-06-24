23032d4c9be1c2b7318b97d09f5dc390ef5dc679
LeHaMinhTrungUIT <18520390@gm.uit.edu.vn>
2021-06-24 12:23:54 +0700

    i

---
+++ app/src/main/java/com/batdaulaptrinh/completedictionary/Category.java
@@ -0,0 +1,33 @@
+package com.batdaulaptrinh.completedictionary;
+
+import androidx.annotation.NonNull;
+
+public class Category {
+    public static final int MEANING = 1;
+    public static final int SYNONYM = 2;
+    public static final int PRONUNCIATION = 3;
+    private int id;
+    private String name;
+    public Category() {
+    }
+    public Category(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @NonNull
+    @Override
+    public String toString() {
+        return getName();
+    }
+}
